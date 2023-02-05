ddfb26a292d37d86d796869c23aaba63bb2af813
Vitaliy Dreko <vitali.dreko@gmail.com>
2023-02-05 17:36:38 +0300

    feat: add telegraf library, add forRootAsync method to inject it, add notify route for review to send review to telegram bot

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -12,15 +12,30 @@ import {
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { UserEmail } from 'src/decorators/user-email/user-email.decorator';
 import { IdValidationPipe } from 'src/pipes/id-validation.pipe';
+import { TelegramService } from 'src/telegram/telegram.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService) {}
+  constructor(
+    private readonly reviewService: ReviewService,
+    private readonly telegramService: TelegramService,
+  ) {}
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
     return this.reviewService.create(dto);
+  }
+
+  @Post('notify')
+  async notify(@Body() dto: CreateReviewDto) {
+    const message =
+      `New Review on ${dto.productId}\n` +
+      `Name:${dto.name}\n` +
+      `Title: ${dto.title}\n` +
+      `Description: ${dto.description}\n` +
+      `Rating: ${dto.rating}`;
+    return this.telegramService.sendMessage(message);
   }
 
   @Delete('delete/:id')
